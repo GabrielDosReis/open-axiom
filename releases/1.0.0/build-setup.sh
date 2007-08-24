d4f4018f1f8c27d063fddcbce62bb176400d18ad
dos-reis <dos-reis@f499efd5-9937-0410-b36a-d3739c31ded8>
2007-08-24 19:12:54 +0000

    Tag OpenAxiom-1.0.0 release.

---
+++ releases/1.0.0/build-setup.sh
@@ -0,0 +1,48 @@
+#! /bin/sh
+
+error() {
+    echo "$1"
+    exit 1
+}
+
+# set -x
+
+notangle ./configure.ac.pamphlet > ./tmp-configure.ac \
+   || error "could not extract configure.ac from pamphlet file"
+config/move-if-change tmp-configure.ac configure.ac
+
+autoheader || error "could not re-generate config/axiom-c-macros.h"
+autoconf || error "could not re-generate configure"
+
+## subdirectories that contain Makefile pamphlets of interest
+SUBDIRS="     .   \
+                  src                    \
+                  src/lib                \
+		  src/lisp               \
+		  src/boot               \
+		  src/interp             \
+		  src/share              \
+		  src/algebra            \
+		  src/etc                \
+		  src/clef               \
+		  src/doc                \
+		  src/graph              \
+                     src/graph/Gdraws    \
+                     src/graph/view2D    \
+                     src/graph/view3D    \
+                     src/graph/viewAlone \
+                     src/graph/viewman   \
+		  src/sman               \
+		  src/hyper              \
+		  src/input              \
+                  src/booklets           \
+"
+
+
+for d in $SUBDIRS; do
+    notangle -t8 $d/Makefile.pamphlet > $d/tmp-Makefile.in \
+       || error "could not extract $d/Makefile.in from pamphlet file"
+    config/move-if-change $d/tmp-Makefile.in $d/Makefile.in
+done
+
+# set +x
