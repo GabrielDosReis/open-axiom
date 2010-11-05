1cfbe5aaa3d2dd322cb6dcc74cab17b784af9328
dos-reis <gdr@axiomatics.org>
2010-11-05 03:50:23 +0000

    * build-setup.sh: Simplify.

--- build-setup.sh
+++ build-setup.sh
@@ -11,15 +11,4 @@ rm -rf autom4te.cache
 autoheader || error "could not re-generate config/openaxiom-c-macros.h"
 autoconf || error "could not re-generate configure"
 
-## subdirectories that contain Makefile pamphlets of interest
-SUBDIRS="		  src/input              \
-"
-
-
-for d in $SUBDIRS; do
-    notangle -t8 $d/Makefile.pamphlet > $d/tmp-Makefile.in \
-       || error "could not extract $d/Makefile.in from pamphlet file"
-    config/move-if-change $d/tmp-Makefile.in $d/Makefile.in
-done
-
 # set +x
