233928c5bd284cd18a2b3eaa3e9774c145ae9c16
Igor Pashev <pashev.igor@gmail.com>
2012-05-10 15:11:24 +0000

    * hyper/initx.c: Fixed spelling errors.     * hyper/macro.c (parse_macro): Likewise.     * sman/sman.c (manage_spad_io): Likewise.

--- src/sman/sman.c
+++ src/sman/sman.c
@@ -665,7 +665,7 @@ manage_spad_io(int ptcNum)
           printf("\n");
           break;
         default:
-          printf("sman: Unkown connection request type: %d\n", p);
+          printf("sman: Unknown connection request type: %d\n", p);
           break;
         }
       }
